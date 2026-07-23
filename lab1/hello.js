fd00aaf65df0bcff2069acb25a31a840d15ba000
Anshita Garg <anshita14garg@gmail.com>
2026-07-23 11:53:54 +0530

    Initial Commit

---
+++ lab1/hello.js
@@ -0,0 +1,17 @@
+// write a function to take two numbers as input and return their sum
+
+// M- 1
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(20, 30));
+
+// M-2
+const add=(a, b) => {
+    return a+b;
+};
+console.log(add(10 , 20));
+
+// M-3
+const add2 = (a , b) => a+b ;
+console.log(add2(40 , 20));
