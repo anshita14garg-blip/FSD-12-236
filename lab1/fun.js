9a226345cb5a34de4bcde7f193f7a59400727e94
Anshita Garg <anshita14garg@gmail.com>
2026-07-23 12:20:04 +0530

    add

---
+++ lab1/fun.js
@@ -0,0 +1,20 @@
+// waf  to take any digit (0-9) and return its in word form 
+
+function digitToWords(digit) {
+    const words ={
+        0: "Zero" ,
+        1: "One", 
+        2:"Two",
+        3:"Three" , 
+        4:"Four", 
+        5:"Five" ,
+        6:"Six" , 
+        7:"Seven" ,
+        8:"Eight" ,
+        9:"Nine"
+    };
+    return words[digit] || "Invalid digit";
+}
+    console.log((digitToWords(5)));
+    console.log((digitToWords(0)));
+    console.log((digitToWords(9)));
