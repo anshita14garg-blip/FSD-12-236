6ad9cc4132429ba036344765d95675c7aaf8548d
Anshita Garg <anshita14garg@gmail.com>
2026-07-24 15:23:57 +0530

    Realexample

--- lab1/realexample.js
+++ lab1/realexample.js
@@ -44,9 +44,9 @@ orderSystem.on("error", (err) => {
 
 // --- Simulate placing an order ---
 orderSystem.placeOrder({
-  id: "ORD1001",
-  customerName: "Dhanesh Kumar",
-  email: "dhanesh@example.com",
+  id: "ORD1001",    
+  customerName: "Anshita Garg",
+  email: "AnshitaGarg@example.com",
   items: [
     { name: "Wireless Mouse", qty: 1 },
     { name: "Mechanical Keyboard", qty: 1 },
