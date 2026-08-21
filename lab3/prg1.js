05d78917db9b675ad5802675565545f8cfe8f130
Anshita Garg <anshita14garg@gmail.com>
2026-08-21 14:51:54 +0530

    lab3

---
+++ lab3/prg1.js
@@ -0,0 +1,11 @@
+import http from 'http';
+const server = http.createServer()
+
+server.on("request" , (req , res ) => {
+    res.write("Hello from server");
+    res.end();
+});
+
+server.listen( 4444 , () => {
+    console.log("server is running...");
+});
